5aaa08c55573c3ab70cd3c36604d940d249b6197
ajmalali10 <officialajmalali10@gmail.com>
2024-09-10 22:17:34 +0530

    added new api struxture

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
 
 /**
- * product controller
+ *  post controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController("api::product.product", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+
+    const post = await strapi.entityService.findMany(
+      "api::product.product",
+      query
+    );
+
+    return this.transformResponse(post[0]);
+  },
+}));
